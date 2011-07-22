f43ac4d5fed769fa52efda894b1a36a75a51a319
Phillip Marshall <phillip@wizpig64.com>
2011-07-21 18:52:16 -0700

    Gulp!

---
+++ gulp.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# Gulp is owned by Phillip Marshall, pending lisencing. Everything else belongs to Whiskey. Please don't sue me.
+"""Gulp, a video downloader for Whiksey Meda"""
+
+import sys
+import os
+import argparse
+
+import code
+
+
+parser = argparse.ArgumentParser(description=__doc__)
+
+args = parser.parse_args()
+
+
+#Do stuff here...
